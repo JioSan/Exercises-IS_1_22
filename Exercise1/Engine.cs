39f2f4df8174b80b8c05a1bbe7b1306ed974424d
a40296 <a40296@207-1>
2024-04-16 14:07:39 +0500

    Переименование

---
+++ Exercise1/Engine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Engine
+    {
+        private const int STATE_STOPPED = 1;
+        private const int STATE_STARTED = 2;
+        public int engineState;
+
+        public void Start()
+        {
+            Console.Out.WriteLine("Start");
+            engineState = STATE_STARTED;
+        }
+
+        public void Stop()
+        {
+            Console.Out.WriteLine("Stop");
+            engineState = STATE_STOPPED;
+        }
+
+        public string GetState()
+        {
+            string state;
+
+            if (engineState == STATE_STOPPED)
+                state = "Stopped";
+            else if (engineState == STATE_STARTED)
+                state = "Started";
+            else
+                state = "Error";
+
+            return state;
+        }
+    }
+}
